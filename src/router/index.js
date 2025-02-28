984ae6908a1f8430919e23dc02271e90b0d8a06a
MorsHs <skyblade163@gmail.com>
2025-03-01 03:16:01 +0800

    route changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import TicketsView from '../views/TicketsMainPage/TicketsView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +10,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: TicketsView,
     },
     {
       path: '/login',
@@ -15,7 +18,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue'),
+      component: LoginView,
     },
     {
       path: '/register',
@@ -23,7 +26,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RegisterView.vue'),
+      component:RegisterView,
     },
     {
       path: '/mainPage',
@@ -31,7 +34,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/TicketsMainPage/TicketsView.vue'),
+      component: TicketsView,
     },
   ],
 })
