fa09c458e59cec12a3a72d961970609b129c26c2
MorsHs <skyblade163@gmail.com>
2025-03-01 05:02:54 +0800

    image and stream

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import TicketsView from '../views/TicketsMainPage/TicketsView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import GuardView from '@/views/Guard/GuardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: TicketsView,
     },
+    {
+      path: '/guard',
+      name: 'guard',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: GuardView,
+    }
   ],
 })
 
---
+++ src/views/Guard/GuardView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts" >
+import { ref } from 'vue'
+import QrcodeVue from 'qrcode.vue'
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+const value = ref('i love java')
+const size = ref(200)
+const qrScanned = ref('')
+const onDetect = (detectedCodes) => {
+  console.log('QR Code detected:', detectedCodes[0]?.rawValue)
+  qrScanned.value = detectedCodes[0]?.rawValue
+}
+</script>
+
+<template>
+  <qrcode-vue :value="value" :size="size" level="H" render-as="svg" />
+  <p>{{ qrScanned }}</p>
+  <QrcodeStream @detect="onDetect" />
+</template>
